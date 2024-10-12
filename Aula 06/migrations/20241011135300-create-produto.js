471671ce45f08abd0ca4f8ae4e5a9d9870058b6b
2022esther <106917457+2022esther@users.noreply.github.com>
2024-10-11 22:49:52 -0300

    Update 20241011135300-create-produto.js

--- Aula 06/migrations/20241011135300-create-produto.js
+++ Aula 06/migrations/20241011135300-create-produto.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       preco: {
-        type: Sequelize.NUMBER
+        type: Sequelize.FLOAT
       },
       createdAt: {
         allowNull: false,
